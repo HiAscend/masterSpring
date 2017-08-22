b7276bc7cc1171690191c02fd5d6682f19e7c499
HiAscend <poseidon1237@163.com>
2017-08-22 14:36:30 +0800

    练习完成

--- chapter2/src/main/java/com/smart/web/LoginController.java
+++ chapter2/src/main/java/com/smart/web/LoginController.java
@@ -1,9 +1,15 @@
 package com.smart.web;
 
+import com.smart.domain.User;
 import com.smart.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.WebParam;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 /**
  * LoginController
@@ -18,8 +24,20 @@ public class LoginController {
         return "login";
     }
 
-
-
+    @RequestMapping(value = "/loginCheck.html")
+    public ModelAndView loginCheck(HttpServletRequest request, LoginCommand loginCommand) {
+        boolean isValidUser = userService.hasMatchUser(loginCommand.getUserName(), loginCommand.getPassword());
+        if (!isValidUser) {
+            return new ModelAndView("login", "error", "用户名和密码错误");
+        } else {
+            User user = userService.findUserByUserName(loginCommand.getUserName());
+            user.setLastIp(request.getLocalAddr());
+            user.setLastVisit(new Date());
+            userService.loginSuccess(user);
+            request.getSession().setAttribute("user", user);
+            return new ModelAndView("main");
+        }
+    }
 
     @Autowired
     public void setUserService(UserService userService) {
