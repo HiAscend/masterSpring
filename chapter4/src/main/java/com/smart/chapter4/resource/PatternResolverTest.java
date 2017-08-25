fc36e62b9f6ccd8b3f61eb848a27567876105204
HiAscend <poseidon1237@163.com>
2017-08-25 14:12:06 +0800

    添加练习

--- chapter4/src/main/java/com/smart/chapter4/resource/PatternResolverTest.java
+++ chapter4/src/main/java/com/smart/chapter4/resource/PatternResolverTest.java
@@ -1,8 +1,12 @@
 package com.smart.chapter4.resource;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+
+import java.io.IOException;
 
 /**
  * Test
@@ -10,8 +14,13 @@ import org.springframework.core.io.support.ResourcePatternResolver;
  */
 public class PatternResolverTest {
     @Test
-    public void getResources() {
+    public void getResources() throws IOException {
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-
+        //加载所有类包com.smart(及子孙包)下.xml为后缀的资源
+        Resource[] resources = resolver.getResources("classpath*:com/smart/**/*.xml");
+        Assert.assertNotNull(resources);
+        for (Resource resource : resources) {
+            System.out.println(resource.getDescription());
+        }
     }
 }
