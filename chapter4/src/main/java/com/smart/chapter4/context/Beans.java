94013130a9a574e78b4936a870f0f4cf2a925456
HiAscend <poseidon1237@163.com>
2017-09-26 22:11:44 +0800

    change

--- chapter4/src/main/java/com/smart/chapter4/context/Beans.java
+++ chapter4/src/main/java/com/smart/chapter4/context/Beans.java
@@ -15,6 +15,7 @@ public class Beans {
     public Car buildCar() {
         Car car = new Car();
         car.setBrand("红旗CA72");
+        car.setColor("黑色");
         car.setMaxSpeed(200);
         return car;
     }
---
+++ chapter4/src/test/java/com/smart/ClassTest.java
@@ -0,0 +1,14 @@
+package com.smart;
+
+import org.testng.annotations.Test;
+
+/**
+ * Test
+ * Created by zziaa on 2017/09/26.
+ */
+public class ClassTest {
+    @Test
+    public void test() {
+        System.out.println(ClassTest.class.getName());
+    }
+}
