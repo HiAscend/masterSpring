3e3ec6b0fac47a6c86d1463e30c0b185f3d4b13f
HiAscend <poseidon1237@163.com>
2017-10-30 17:15:09 +0800

    chapter7

---
+++ chapter7/src/main/java/com/smart/chapter7/proxy/ForumServiceImpl.java
@@ -0,0 +1,19 @@
+package com.smart.chapter7.proxy;
+
+/**
+ * ForumServiceImpl
+ *
+ * @author ascend
+ * @date 2017/10/30 17:14
+ */
+public class ForumServiceImpl implements ForumService {
+    @Override
+    public void removeTopic(int topicId) {
+
+    }
+
+    @Override
+    public void removeForum(int forumId) {
+
+    }
+}
---
+++ chapter7/src/main/java/com/smart/chapter7/proxy/PerformanceMonitor.java
@@ -0,0 +1,11 @@
+package com.smart.chapter7.proxy;
+
+/**
+ * PerformanceMonitor
+ *
+ * @author ascend
+ * @date 2017/10/30 17:14
+ */
+public class PerformanceMonitor {
+
+}
