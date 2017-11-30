4cea088a24649b2a0b93d4fe3dd9f2cc23b21c6a
HiAscend <poseidon1237@163.com>
2017-11-30 22:21:57 +0800

    新增chapter15

---
+++ chapter15/src/main/java/com/smart/chapter15/cache/simplecache/UserMain.java
@@ -0,0 +1,26 @@
+package com.smart.chapter15.cache.simplecache;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * UserMain
+ *
+ * @author ascend
+ * @date 2017/11/30 22:01
+ */
+public class UserMain {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+
+        // 第一次查询
+        System.out.println("first query...");
+        userService.getUserById("somebody");
+
+        // 第二次查询，应该不查询数据库，直接返回缓存中的指
+        System.out.println("second query...");
+        userService.getUserById("somebody");
+        userService.getUserById("somebodyelse");
+    }
+}
