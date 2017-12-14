52319559a6b8d7c893c6a41e96e1decbf8505c7d
HiAscend <poseidon1237@163.com>
2017-12-14 22:16:52 +0800

    练习

---
+++ chapter15/src/main/java/com/smart/chapter15/cache/hazelcast/UserMain.java
@@ -0,0 +1,30 @@
+package com.smart.chapter15.cache.hazelcast;
+
+import com.smart.chapter15.cache.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * UserMain
+ *
+ * @author ascend
+ * @date 2017/12/14 22:10
+ */
+public class UserMain {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-hazelcast.xml");
+        UserService userService = (UserService) context.getBean("hazelcastUserService");
+
+        User user1 = new User("2", "w2", 34);
+        User userFetch1 = userService.getUser(user1);
+        System.out.println(userFetch1);
+        User userFetch2 = userService.getUser(user1);
+        System.out.println(userFetch2);
+
+        User user2 = new User("1", "w1",37);
+        User userFetch3 = userService.getUser(user2);
+        System.out.println(userFetch3);
+        User userFetch4 = userService.getUser(user2);
+        System.out.println(userFetch4);
+    }
+}
---
+++ chapter15/src/main/java/com/smart/chapter15/cache/hazelcast/UserService.java
@@ -0,0 +1,29 @@
+package com.smart.chapter15.cache.hazelcast;
+
+import com.smart.chapter15.cache.domain.User;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * UserService
+ *
+ * @author ascend
+ * @date 2017/12/14 22:10
+ */
+@Service(value = "hazelcastUserService")
+public class UserService {
+    private Map<Integer, User> users = new HashMap<Integer, User>();
+
+    {
+        users.put(1, new User("1", "w1", 37));
+        users.put(2, new User("2", "w2", 34));
+    }
+    @Cacheable(value = "users", condition = "#user.age < 35")
+    public User getUser(User user) {
+        System.out.println("User with id " + user.getUserId() + " requested.");
+        return users.get(Integer.valueOf(user.getUserId()));
+    }
+}
