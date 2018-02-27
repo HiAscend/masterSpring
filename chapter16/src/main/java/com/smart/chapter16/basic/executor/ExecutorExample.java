e58af64058f6f359ed895c6d6e44f290f10ad543
HiAscend <poseidon1237@163.com>
2018-02-27 22:26:19 +0800

    quartz练习

--- chapter16/src/main/java/com/smart/chapter16/basic/executor/ExecutorExample.java
+++ chapter16/src/main/java/com/smart/chapter16/basic/executor/ExecutorExample.java
@@ -1,5 +1,8 @@
 package com.smart.chapter16.basic.executor;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 /**
  * ExecutorExample
  *
@@ -7,5 +10,40 @@ package com.smart.chapter16.basic.executor;
  * @date 2018/02/27 19:20
  */
 public class ExecutorExample {
+    /**
+     * 声明一个执行器
+     */
+    private Executor executor;
 
+    public void setExecutor(Executor executor) {
+        this.executor = executor;
+    }
+
+    /**
+     * 用执行器执行多个任务
+     */
+    public void executeTasks() {
+        for (int i = 0; i < 6; i++) {
+            executor.execute(new SimpleTask("task" + i));
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorExample ee = new ExecutorExample();
+        ee.setExecutor(Executors.newFixedThreadPool(3));
+        ee.executeTasks();
+    }
+}
+
+class SimpleTask implements Runnable {
+    private String taskName;
+
+    public SimpleTask(String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("do " + taskName + " ... in Thread:" + Thread.currentThread().getId());
+    }
 }
