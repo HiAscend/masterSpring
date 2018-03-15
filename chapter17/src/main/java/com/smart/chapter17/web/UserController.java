375794ef23b0f816b211ed40cc964b0f8ab2a996
HiAscend <poseidon1237@163.com>
2018-03-15 22:50:01 +0800

    practice

--- chapter17/src/main/java/com/smart/chapter17/web/UserController.java
+++ chapter17/src/main/java/com/smart/chapter17/web/UserController.java
@@ -9,6 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
@@ -185,6 +188,22 @@ public class UserController {
         Resource resource = new ClassPathResource("image.png");
         return IOUtils.toByteArray(resource.getInputStream());
     }
+
+    @RequestMapping(path = "/handle44/{imageId}")
+    public ResponseEntity<byte[]> handle44(@PathVariable("imageId") String imageId) throws IOException {
+        LOG.debug("load image id:{}", imageId);
+        Resource resource = new ClassPathResource("/image.png");
+        byte[] fileData = FileCopyUtils.copyToByteArray(resource.getFile());
+        return new ResponseEntity<>(fileData, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/handle51")
+    public ResponseEntity<User> handle51(HttpEntity<User> requestEntity) {
+        User user = requestEntity.getBody();
+        user.setUserId("10086");
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
 
 
 }
--- chapter17/src/test/java/com/smart/chapter17/web/UserControllerTest.java
+++ chapter17/src/test/java/com/smart/chapter17/web/UserControllerTest.java
@@ -1,9 +1,21 @@
 package com.smart.chapter17.web;
 
+import com.smart.chapter17.domain.User;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 /**
  * UserControllerTest
@@ -20,5 +32,46 @@ public class UserControllerTest {
         form.add("password", "123456");
         form.add("age", "45");
         restTemplate.postForLocation("http://localhost:8080/chapter17/user/handle41.html", form);
+    }
+
+    @Test
+    public void testHandle44() throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        byte[] response = restTemplate.postForObject("http://localhost:8080/chapter17/user/handle44/{itemId}.html", null, byte[].class, "1233");
+        Resource outFile = new FileSystemResource("/tmp/image_copy.png");
+        FileCopyUtils.copy(response, outFile.getFile());
+    }
+
+    @Test
+    public void testHandle51WithXml() {
+        RestTemplate restTemplate = buildRestTemplate();
+
+        User user = new User();
+        user.setUserName("tom");
+        user.setPassword("123456");
+        user.setRealName("汤姆");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_XML);
+
+    }
+
+    private RestTemplate buildRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        // 创建MarshallingHttpMessageConverter
+        XStreamMarshaller xStreamMarshaller = new XStreamMarshaller();
+        xStreamMarshaller.setStreamDriver(new StaxDriver());
+        xStreamMarshaller.setAnnotatedClasses(User.class);
+
+        MarshallingHttpMessageConverter xmlConverter = new MarshallingHttpMessageConverter();
+        xmlConverter.setMarshaller(xStreamMarshaller);
+        xmlConverter.setUnmarshaller(xStreamMarshaller);
+
+        restTemplate.getMessageConverters().add(xmlConverter);
+
+        // 创建MappingJacksonHttpMessageConverter
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        restTemplate.getMessageConverters().add(jsonConverter);
+        return restTemplate;
     }
 }
