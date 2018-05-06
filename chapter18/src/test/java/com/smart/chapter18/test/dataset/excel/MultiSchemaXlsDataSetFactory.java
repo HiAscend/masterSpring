d88bc8b58f741da22298d19e55995f67f6e0a7e8
HiAscend <poseidon1237@163.com>
2018-05-06 11:43:48 +0800

    dao层配置相关

---
+++ chapter18/src/test/java/com/smart/chapter18/test/dataset/excel/MultiSchemaXlsDataSetFactory.java
@@ -0,0 +1,42 @@
+package com.smart.chapter18.test.dataset.excel;
+
+import org.unitils.core.UnitilsException;
+import org.unitils.dbunit.datasetfactory.DataSetFactory;
+import org.unitils.dbunit.util.MultiSchemaDataSet;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * MultiSchemaXlsDataSetFactory
+ *
+ * @author zziaa
+ * @date 2018/05/06 11:02
+ */
+public class MultiSchemaXlsDataSetFactory implements DataSetFactory {
+
+    protected String defaultSchemaName;
+
+    @Override
+    public void init(Properties properties, String s) {
+        this.defaultSchemaName = s;
+    }
+
+    @Override
+    public MultiSchemaDataSet createDataSet(File... files) {
+        try {
+            MultiSchemaXlsDataSetReader xlsDataSetReader = new MultiSchemaXlsDataSetReader(
+                defaultSchemaName);
+            return xlsDataSetReader.readDataSetXls(dataSetFiles);
+        } catch (Exception e) {
+            throw new UnitilsException("创建数据集失败: "
+                + Arrays.toString(dataSetFiles), e);
+        }
+    }
+
+    @Override
+    public String getDataSetFileExtension() {
+        return null;
+    }
+}
---
+++ chapter18/src/test/java/com/smart/chapter18/test/dataset/excel/MultiSchemaXlsDataSetReader.java
@@ -0,0 +1,11 @@
+package com.smart.chapter18.test.dataset.excel;
+
+/**
+ * MultiSchemaXlsDataSetReader
+ *
+ * @author zziaa
+ * @date 2018/05/06 11:21
+ */
+public class MultiSchemaXlsDataSetReader {
+
+}
