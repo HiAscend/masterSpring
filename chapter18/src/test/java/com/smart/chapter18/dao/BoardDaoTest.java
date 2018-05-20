196fc4248127ae9fe2a6562bf7d0b616b0487885
HiAscend <poseidon1237@163.com>
2018-05-20 22:52:24 +0800

    dao层配置相关

--- chapter18/src/test/java/com/smart/chapter18/dao/BoardDaoTest.java
+++ chapter18/src/test/java/com/smart/chapter18/dao/BoardDaoTest.java
@@ -3,10 +3,7 @@ package com.smart.chapter18.dao;
 import com.smart.chapter18.domain.Board;
 import com.smart.chapter18.test.dataset.util.XlsDataSetBeanFactory;
 import org.testng.annotations.Test;
-import org.unitils.UnitilsTestNG;
-import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.dbunit.annotation.ExpectedDataSet;
-import org.unitils.spring.annotation.SpringApplicationContext;
 import org.unitils.spring.annotation.SpringBean;
 
 import java.util.List;
@@ -17,8 +14,8 @@ import java.util.List;
  * @author ascend
  * @date 2018/5/10 16:07.
  */
-@SpringApplicationContext(value = {"xiaochun-dao.xml"})
-public class BoardDaoTest extends UnitilsTestNG {
+public class BoardDaoTest extends BaseDaoTest {
+
     @SpringBean("boardDao")
     private BoardDao boardDao;
 
@@ -26,7 +23,6 @@ public class BoardDaoTest extends UnitilsTestNG {
      * 创建一个新的论坛版块,并更新
      */
     @Test
-    @DataSet("XiaoChun.SaveBoards.xls")
     @ExpectedDataSet("XiaoChun.SaveBoards.xls")
     public void addBoard() throws Exception {
         // 通过XlsDataSetBeanFactory数据集绑定工厂创建测试实体
